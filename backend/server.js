1b3f0e37050f13952a8bae5751a73bad76166d22
Rob DiVincenzo <rob.divincenzo@gmail.com>
2022-11-11 17:21:07 -0500

    Test API endpoint

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,12 @@
-console.log('Hello World');
+const express = require('express');
+const dotenv = require('dotenv').config();
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/api/goals', (req, res) => {
+    res.status(200).json({"message": "Get goals"});
+});
+ 
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`)
+});
