ccf56907b610a9989327eb86059822beeaa99c0f
Rob DiVincenzo <rob.divincenzo@gmail.com>
2022-11-11 17:48:15 -0500

    Error handle

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -9,6 +9,10 @@ const getGoals = (req, res) => {
 // @route   POST /api/goal/:id
 // @access  Private
 const setGoal = (req, res) => {
+    if(!req.body.text) {
+        res.status(400);
+        throw new Error('Please add a text field');
+    }
     console.log(req.body);
     res.status(200).json({"message": "Set goals"});
 }
