d971c87078ddf6c85ebb6ec4f213a54cef6443a4
Rob DiVincenzo <rob.divincenzo@gmail.com>
2022-11-11 22:49:24 -0500

    Middleware custom error messaging

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
+const { errorHandler } = require('./middleware/errorMiddleware');
+
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -8,6 +10,7 @@ app.use(express.urlencoded({extended: false}));
 
 app.use('/api/goals', require('./routes/goalRoutes'));
 
+app.use( errorHandler );
 app.listen(port, () => {
     console.log(`Server started on port ${port}`)
 });
